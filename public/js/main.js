02cb035e127cb5ba6f97bf06708b54e33c9a2d11
viveur <ivebeentherealready@proton.me>
2024-04-08 13:27:10 +0300

    fix: eggInput enter onclick red warning, 100.5% width for chickens-image.

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,14 @@
 $(() => {
   //- ENTER BUTTON TRIGGER EGGS INPUT
   $("input[name='eggQuantity']").on('keydown', (e) => {
-    if (e.which === 13)
-      $(".OP-add-to-cart").trigger('click')
+    if (e.which === 13) {
+      $(".OP-add-to-cart").trigger('click') 
+      $("input[name='eggQuantity'").addClass('warningFocus')
+      setTimeout(() => {
+      $("input[name='eggQuantity'").removeClass('warningFocus')
+
+      },1000)
+    }
   })
 
   // ESC BUTTON TRIGGER X ORDER PREVIEW
